609b9abf239574ba95c64d38e65071f419f16ad0
Herbin Esteban <herbin.estebanSIO@gmail.com>
2022-07-06 02:51:55 +0200

    Rajout des entités symfony

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,56 @@
 
 namespace App\Controller;
 
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilder;
+use Symfony\UX\Chartjs\Model\Chart;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
+        $chartBuilder = new ChartBuilder();
+        // Option 1. You can make your dashboard redirect to some common page of your backend
+        //
+        //return $this->redirect($adminUrlGenerator->setController(UsersCrudController::class)->generateUrl());
+
+        // Option 2. You can make your dashboard redirect to different pages depending on the user
+        //
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirect('...');
+        // }
+
+        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
+        // ...set chart data and options somehow
+        $chart->setData([
+            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'datasets' => [
+                [
+                    'label' => 'My First dataset',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [0, 10, 5, 2, 20, 30, 45],
+                ],
+            ],
+        ]);
+        $chart->setOptions([
+            'scales' => [
+                'y' => [
+                    'suggestedMin' => 0,
+                    'suggestedMax' => 100,
+                ],
+            ],
+        ]);
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'chart'=>$chart
         ]);
     }
 }
